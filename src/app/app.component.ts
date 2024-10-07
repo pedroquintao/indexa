9fafbe88fd7a207efdcc4f94999de07fa9e03729
pedroquintao <pedro.h.quintao@hotmail.com>
2024-10-07 07:25:16 -0300

    Versão inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'indexa';
